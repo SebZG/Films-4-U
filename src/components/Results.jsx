dbbb7ce75dd62f2912230666a3e543fabaf8f512
SebZG <sebastian.zapata.g@gmail.com>
2023-12-26 21:14:22 +0000

    Search function working and added Loading skeleton

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -1,26 +1,74 @@
 import React, { useEffect, useState } from "react";
+import axios from "axios";
 
-import harry from "../images/harry.jpg";
+import HomeImage from "./HomeImage";
 
 const Results = ({ searchQuery }) => {
 	const [results, setResults] = useState([]);
+	const [loading, setLoading] = useState(false);
+	const [error, setError] = useState(null);
+
+	async function getMovies() {
+		try {
+			setLoading(true);
+
+			const apiKEY = "fe2dc3ba";
+			const apiURL = `https://www.omdbapi.com/?apikey=${apiKEY}&s=${searchQuery}`;
+
+			const { data } = await axios.get(apiURL);
+			if (data && data.Search) {
+				setResults(data.Search);
+			}
+		} catch (error) {
+			setError("Could not find your movie. Please try again.");
+			console.error(error);
+		} finally {
+			setTimeout(() => {
+				setLoading(false);
+			}, 1000);
+		}
+	}
 
 	useEffect(() => {
-		// Fetch the search results using the searchQuery prop
-		// Update the results state with the fetched data
-		// You can use the fetch API or any other HTTP library like Axios
+		getMovies();
 	}, [searchQuery]);
 
 	return (
-		<div className="card" id="results" style={{ width: "18rem" }}>
-			<div
-				className="card-img-top"
-				alt="Movie"
-				style={{ backgroundImage: `url(${harry})` }}></div>
-			<ul class="list-group list-group-flush">
-				<li class="list-group-item">Title</li>
-				<li class="list-group-item">Year</li>
-			</ul>
+		<div id="results">
+			{results.length === 0 ? (
+				<HomeImage />
+			) : loading ? (
+				new Array(8).fill(0).map((_, index) => (
+					<div key={index} className="card skeleton">
+						<div className="card-img-top" alt="Movie">
+							<div className="poster__skeleton"></div>
+						</div>
+						<ul className="list-group list-group-flush">
+							<li className="list-group-item">
+								<div className="title__skeleton"></div>
+							</li>
+							<li className="list-group-item">
+								<div className="year__skeleton"></div>
+							</li>
+						</ul>
+					</div>
+				))
+			) : error ? (
+				<div>Error: {error}</div>
+			) : (
+				results.slice(0, 8).map((movie) => (
+					<div key={movie.imdbID} className="card results">
+						<div
+							className="card-img-top"
+							alt={movie.Title}
+							style={{ backgroundImage: `url(${movie.Poster})` }}></div>
+						<ul className="list-group list-group-flush">
+							<li className="list-group-item">{movie.Title}</li>
+							<li className="list-group-item">{movie.Year}</li>
+						</ul>
+					</div>
+				))
+			)}
 		</div>
 	);
 };
